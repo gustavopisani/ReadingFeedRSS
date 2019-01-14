fe412740212e083270492520048b72f633002032
Gustavo Pisani da Silva <gustavo.silva@fcamara.com.br>
2019-01-14 17:52:03 -0200

    layout front finalizado

--- WebFeedRSS/Controllers/HomeController.cs
+++ WebFeedRSS/Controllers/HomeController.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebFeedRSS.Models;
 
@@ -12,20 +10,6 @@ namespace WebFeedRSS.Controllers
     {
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
